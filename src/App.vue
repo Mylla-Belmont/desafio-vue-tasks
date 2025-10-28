17f4f273eb8a813776083e4fc8a03d084b1ef7cd
Camila Belmont <camilabelmont.alu@gmail.com>
2025-10-28 12:26:19 -0300

    Adicionando logo com light theme e dark theme

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,32 @@ function onClick() {
 </script>
 
 <template>
-  <v-responsive class="border rounded" max-height="300">
+  <v-responsive>
     <v-app :theme="theme">
       <v-row>
         <v-app-bar class="pr-3">
-          <v-app-bar-title>
-            <v-img :src="theme === 'light' ? logo1 : logo2"
-              :srcset="theme === 'light' ? `${logo1} 1x, ${logo1} 2x` : `${logo2} 1x, ${logo2} 2x`" width="60"
-              height="60"></v-img>
-          </v-app-bar-title>
-          <v-btn :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" text slim
-            @click="onClick">
-            Dark Mode
-          </v-btn>
+          <v-row align="center" no-gutters class="ma-0 pa-0 flex-grow-1">
+            <v-col cols="auto" class="pa-0 pl-2">
+              <v-img :src="theme === 'light' ? logo1 : logo2" width="60" height="60" contain />
+            </v-col>
+
+            <v-col cols="auto" class="pa-0 ml-2">
+              <v-app-bar-title>
+                Kanban Vue.js
+              </v-app-bar-title>
+            </v-col>
+
+            <v-spacer></v-spacer>
+
+            <v-col cols="auto" class="pa-0">
+              <v-btn :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" text slim
+                @click="onClick">
+                {{ theme === 'light' ? 'Dark Mode' : 'Light Mode' }}
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-app-bar>
+
       </v-row>
 
       <v-main>
